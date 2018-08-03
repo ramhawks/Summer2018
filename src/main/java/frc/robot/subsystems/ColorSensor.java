41939d1221ac7df98cb213f178573209e33496a9
ramhawks <winonaramhawks@gmail.com>
2018-08-02 21:10:40 -0500

    began color sensor subsystem

---
+++ src/main/java/frc/robot/subsystems/ColorSensor.java
@@ -0,0 +1,29 @@
+/*----------------------------------------------------------------------------*/
+/* Copyright (c) 2018 FIRST. All Rights Reserved.                             */
+/* Open Source Software - may be modified and shared by FRC teams. The code   */
+/* must be accompanied by the FIRST BSD license file in the root directory of */
+/* the project.                                                               */
+/*----------------------------------------------------------------------------*/
+
+package frc.robot.subsystems;
+
+import edu.wpi.first.wpilibj.I2C;
+import edu.wpi.first.wpilibj.command.Subsystem;
+
+/**
+ * Add your docs here.
+ */
+public class ColorSensor extends Subsystem {
+  
+  I2C colorSensor = new I2C(I2C.Port.kOnboard, 0x3C);
+
+  public int getColor(){
+      
+  }
+
+  @Override
+  public void initDefaultCommand() {
+    // Set the default command for a subsystem here.
+    // setDefaultCommand(new MySpecialCommand());
+  }
+}
